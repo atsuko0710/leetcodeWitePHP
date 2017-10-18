ea407e861ea995f5b74bf6a867229eb646739a6b
atsukodan <atsukodan10@gmail.com>
2017-10-18 23:11:16 +0800

    21

---
+++ easy/MergeTwoSortedLists.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * Merge two sorted linked lists and return it as a new list. 
+ * The new list should be made by splicing together the nodes of the first two lists.
+ * 合并两个排序的链接列表，并将其作为新列表返回。
+ * 新列表应通过拼接前两个列表的节点进行。
+ */
+	
+	
